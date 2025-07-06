fb990c7e4f707baa52c56b86abac9f31b3b7255b
Dayeon Roh <dayeonroh102@gmail.com>
2025-07-06 22:29:18 +0900

    Change home route to render index.html

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Render!"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
