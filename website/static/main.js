4334243379f2c8459e8bcc7a02f485ffaf4563e5
Dayeon Roh <dayeonroh102@gmail.com>
2025-07-06 22:11:16 +0900

    Add image prediction and Flask route update

---
+++ website/static/main.js
@@ -0,0 +1,12 @@
+async function sendInput() {
+    const input = document.getElementById("inputBox").value;
+
+    const response = await fetch("/predict", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ input: input })
+    });
+
+    const data = await response.json();
+    document.getElementById("resultBox").innerText = data.result;
+}
