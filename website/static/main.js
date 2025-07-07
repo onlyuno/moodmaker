47c451ba93b9ca72eaea24c8d936437d9c97bcd3
Dayeon Roh <dayeonroh102@gmail.com>
2025-07-07 12:34:58 +0900

    Fix: main.js and index.html

--- website/static/main.js
+++ website/static/main.js
@@ -15,6 +15,11 @@ async function sendImage() {
         body: formData
     });
 
-    const html = await response.text();
-    document.getElementById("resultBox").innerHTML = html;
+    if (!response.ok) {
+        document.getElementById("resultBox").innerHTML = "분석 실패. 서버 응답 오류.";
+        return;
+    }
+
+    const result = await response.json();
+    document.getElementById("resultBox").innerHTML = `분석된 장소는 <strong>${result.prediction}</strong>입니다.`;
 }
