65266804614dd4de37ec9289dfb2f341bdd9128f
Dayeon Roh <dayeonroh102@gmail.com>
2025-07-08 19:53:34 +0900

    Fix: 드디어 스포티파이 연결 가나요

--- website/static/main.js
+++ website/static/main.js
@@ -22,4 +22,13 @@ async function sendImage() {
 
     const result = await response.json();
     document.getElementById("resultBox").innerHTML = `분석된 장소는 <strong>${result.prediction}</strong>입니다.`;
+
+    if (result.tracks && result.tracks.length > 0) {
+    let trackList = "<ul>";
+    result.tracks.forEach(track => {
+        trackList += `<li><a href="${track.url}" target="_blank">${track.name} - ${track.artist}</a></li>`;
+    });
+    trackList += "</ul>";
+    document.getElementById("resultBox").innerHTML += `<div>추천 음악:<br>${trackList}</div>`;
+    }
 }
