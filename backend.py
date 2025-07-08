08befec2e301a863b2bface9d6bc54585d53b9f7
Dayeon Roh <dayeonroh102@gmail.com>
2025-07-08 20:39:19 +0900

    add .cache, app.yaml and fix backend.py

--- backend.py
+++ backend.py
@@ -103,5 +103,6 @@ def predict():
     })
 
 # 서버 실행
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # 기본 8080
+    app.run(host="0.0.0.0", port=port)
